d2a1f695099f7eb58a1978dfa488819c9c93ad83
BXS-Archipelago <bxs@tutanota.com>
2021-06-29 19:50:50 +0000

    update checkout address fields

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 from .models import Order
 
-class OrderForm():
+class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ('full_name','email', 'phone_number',
+        fields = ('full_name', 'email', 'phone_number',
                   'street_address1', 'street_address2',
                   'town_or_city', 'postcode', 'country',
                   'county',)
@@ -36,4 +36,3 @@ class OrderForm():
             self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
-
