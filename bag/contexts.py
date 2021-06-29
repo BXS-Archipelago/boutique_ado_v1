d7a57919deeacbaca38d1989e78cffffb25fff9c
BXS-Archipelago <bxs@tutanota.com>
2021-06-29 09:50:28 +0000

    Added toasts

--- bag/contexts.py
+++ bag/contexts.py
@@ -12,7 +12,7 @@ def bag_contents(request):
 
     for item_id, item_data in bag.items():
         if isinstance(item_data, int):
-            product = get_object_or_404(Product,pk=item_id)
+            product = get_object_or_404(Product, pk=item_id)
             total += item_data * product.price
             bag_items.append({
                 'item_id': item_id,
@@ -20,7 +20,7 @@ def bag_contents(request):
                 'product': product
             })
         else: 
-            product = get_object_or_404(Product,pk=item_id)
+            product = get_object_or_404(Product, pk=item_id)
             for size, quantity in item_data['items_by_size'].items():
                 total += quantity * product.price
                 product_count += quantity
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from products.models import Product
 
 # Create your views here.
 
@@ -10,6 +12,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -30,6 +33,32 @@ def add_to_bag(request, item_id):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag 
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """" Adjust quantity of the specified product in the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST: 
+        size = request.POST['product_size']        
+    bag = request.session.get('bag', {})
+
+    if size:
+        if quantity > 0:
+            bag[item_id]['items_by_size'][size] = quantity
+        else: 
+            del bag[item_id]['items_by_size'][size]
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else: 
+            bag.pop[item_id]
+
+    request.session['bag'] = bag 
+    return redirect(reverse('view_bag'))
+
