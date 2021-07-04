9785853949d066728b57cc4a5deef683b6cb9b8a
BXS-Archipelago <bxs@tutanota.com>
2021-07-04 21:20:31 +0000

    update AWS key environ get

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -189,8 +189,10 @@ if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'bxs-boutique-ado'
     AWS_S3_REGION_NAME = 'eu-west-1'
-    AWS_ACCESS_KEY_ID = os.environ('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ('AWS_SECRET_ACCESS_KEY')
+    
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
